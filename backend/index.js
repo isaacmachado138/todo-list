863370e747919438df7a31da3de202b10eaaa496
isaac.machado <isaac.machado@35.168.70.227>
2024-07-16 01:49:48 -0300

    fix: Corrigindo bugs na conteinerização

--- backend/index.js
+++ backend/index.js
@@ -13,14 +13,14 @@ app.use(cors());
 app.use('/tasks', taskRoutes);
 
 // Teste de conexão com o banco de dados
-db.query('SELECT NOW()', (err, res) => {
+/*db.query('SELECT NOW()', (err, res) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados:', err.stack);
     } else {
         console.log('Conexão com o banco de dados bem-sucedida.');
         console.log('Hora atual do banco de dados:', res.rows[0].now);
     }
-});
+});*/
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
